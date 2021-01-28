7048b0d564e301600325c21f0f0bdb2d66736d48
hungngk98 <hungngk98@gmail.com>
2021-01-29 06:42:59 +0700

    day 3

--- homework-3/bai2/app.js
+++ homework-3/bai2/app.js
@@ -2,9 +2,10 @@ import Person from './Person.js';
 import findRelatives from './findRelatives.js';
 
 // let hung = new Person('Hung', 22, ['Linh', 'Duc']);
-// let duc = new Person('Duc', 23, ['Hung', 'Minh']);
+// let duc = new Person('Duc', 23, ['Hung', 'Minh', 'Kien', 'Linh']);
 // let linh = new Person('Linh', 19, ['Duc', 'Hung']);
 // let minh = new Person('Minh', 30, ['Duc']);
-// let kien = new Person('Kien', 21, ['Duc']);
+// let kien = new Person('Kien', 21, ['Duc','Long']);
+// let long = new Person('Long', 21, ['Kien']);
 
-findRelatives();
+findRelatives('Hung');
--- homework-3/bai2/findRelatives.js
+++ homework-3/bai2/findRelatives.js
@@ -2,7 +2,28 @@ import Person from './Person.js';
 
 export default async function findRelatives(name) {
     let allPeople = await firebase.firestore().collection('people').get();
+    let queue = [];
+    let confirmed = [];
     for (let person of allPeople.docs) {
-        console.log(person.data().relatives);
+        if (person.data().name == name) {
+            confirmed.push(name);
+            for (let i = 0; i < person.data().relatives.length; i++) {
+                confirmed.push(person.data().relatives[i]);
+                queue.push(person.data().relatives[i]);
+            }
+        }
     }
+    for (let i = 0; i < queue.length; i++) {
+        for (let person of allPeople.docs) {
+            if (person.data().name == queue[i]) {
+                for (let j = 0; j < person.data().relatives.length; j++) {
+                    if (confirmed.includes(person.data().relatives[j]) == false) {
+                        confirmed.push(person.data().relatives[j]);
+                        queue.push(person.data().relatives[j]);
+                    }
+                }
+            }
+        }
+    }
+    console.log('confirmed relatives: ', confirmed);
 }
